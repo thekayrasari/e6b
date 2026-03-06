a3ab50c5e648ac22207ade4dece23ca9c1034f6b
thekayrasari <thekayrasari@gmail.com>
2026-03-06 22:30:53 +0300

    fix e6b capit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import e6b from './e6b.jsx'
+import E6B from './e6b.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <e6b />
+    <E6B />
   </StrictMode>,
 )
