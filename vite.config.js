f161ba5cdfdf13bb53701b743a8135d3655c030c
thekayrasari <thekayrasari@gmail.com>
2026-03-22 23:26:36 +0300

    only html css and js now

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/e6b/',  // must match your GitHub repo name
-  plugins: [react()],
-})
